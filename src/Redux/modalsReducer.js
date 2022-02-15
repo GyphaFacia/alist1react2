159c22e91e6a5a480d8c8d80810f45fa3adf1c2b
Blank <holoparrentattachment@gmail.com>
2022-02-16 00:20:25 +0300

    header and search style fixes

--- src/Redux/modalsReducer.js
+++ src/Redux/modalsReducer.js
@@ -6,10 +6,10 @@ export const setCloseAllModals = (payload)=>({type: 'setCloseAllModals', payload
 
 export const modalsReducer = (state = defaultState, action)=>{
     const {payload, type} = action
-    console.log(type)
-    console.groupCollapsed('show')
-    console.dir(payload)
-    console.groupEnd()
+    // console.log(type)
+    // console.groupCollapsed('show')
+    // console.dir(payload)
+    // console.groupEnd()
     switch (type) {
         case 'setCloseAllModals': return {...state, closeAllModals: payload}
         default:
